1d1cad5b3e4e97f74fb271414388f456fd2aad12
asareli <aracelimj317@gmail.com>
2023-11-15 02:49:46 -0300

    cambios en el html y css. falta dropdown menu

--- Clase 8/clase8.js
+++
@@ -1,7 +0,0 @@
-// ************************ DOM
-console.log(document.body);
-const contenido = document.getElementById("contenido") //acceder al elemento mediante ID
-console.log(contenido);
-const parrafos = document.getElementsByClassName("parrafo");
-
-// ************************
